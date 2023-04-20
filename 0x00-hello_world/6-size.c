f894b77547e428211eff148dc806e9f4ca5b5173
Ran0717 <raniabelmir17@gmail.com>
2023-04-20 15:27:29 -0700

    0x00 C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+/**
+ * main - Program that  prints the size
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsignd long)sizeof(a);
+printf("Size of a int: %lu byte(s)\n", (unsignd long)sizeof(b);
+printf("Size of a long int: %lu bytes(s)\n", (unsignd long)sizeof(c);
+printf("Size of a long long int: %lu bytes(s)\n", (unsignd long)sizeof(d);
+printf("Size of a float: %lu bytes(%)\n", (unsignd long)sizeof(f);
+return (0);
+}
